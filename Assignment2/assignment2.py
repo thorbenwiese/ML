6c8f35610e0e967ea9ca5af2dc72584eb6cd3c97
Thorben Wiese <thorben.wiese@outlook.com>
2018-04-20 20:42:25 +0200

    Assignment 02.4 a) and b) not finished

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,7 +1,10 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
 
 def generateHilbertMatrix(k):
   m = np.zeros((k,k))
@@ -50,6 +53,18 @@ def findMinMaxMean(data):
   print "Max Indices: " + str(maxIdx)
   print "Mean: " + str(mean)
 
+def euclideanDist(set1, set2):
+  dist = 0
+  for i in range(0,len(set1)):
+    dist += pow((set1[i] - set2[i]), 2)
+  return math.sqrt(dist)
+
+def euclideanDistForTwoPoints(point1, point2):
+  return math.sqrt(pow((point1 - point2), 2))
+
+def calculateLoss(set1, set2):
+  return mean_squared_error(set1, set2)
+
 def main():
   # Assignment 02.2
   # a) b)
@@ -96,11 +111,11 @@ def main():
   # d)
   plt.figure()
   plt.title("Geographical map of houses")
-  plt.scatter(data[:,0], data[:,1], s=2, alpha=0.1, c=data[:,0], cmap=cm.bwr)
+  plt.scatter(data[:,0], data[:,1], s=2, alpha=0.1, c=data[:,7], cmap=cm.bwr)
 
   # e)
   labels = range(0,numRows)
-  values = data[:,:-1]
+  values = data
 
   X_train, X_test, y_train, y_test = train_test_split(values, labels, test_size=0.20, random_state=42)
 
@@ -118,7 +133,36 @@ def main():
   # --> the test and training set distribution match
 
   # plots all figures
-  plt.show()
+  #plt.show()
+
+
+  # Assignment 02.4
+  # a)
+  np.random.seed(9876)
+  mod = GradientBoostingRegressor(loss='ls')
+  fit = mod.fit(X_train, y_train)
+  predict = fit.predict(X_test)
+  #print predict
+  #print y_test
+  loss = calculateLoss(predict, y_test)
+  # TODO loss ist viel zu hoch?!
+  print loss
+
+  # b)
+  distance = euclideanDist(data[:,0], data[:,1])
+  # TODO distance ist viel zu groß?!
+  # vielleicht sollte für kNN die Funktion euclideanDistForTwoPoints()
+  # verwendet werden..
+  print 'Distance: ' + repr(distance)
+
+  # c)
+  # implement kNN
+  # siehe https://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+
+
+  # d)
+  # predict housing values for different values of k
+  # what are training and test errors?
 
 # execute main
 if __name__ == "__main__":
