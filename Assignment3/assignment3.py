a9a9657ecfc07cc1927a19705affbedcea346683
Marcel Rohde <2rohde@informatik.uni-hamburg.de#>
2018-04-23 19:01:38 +0200

    ansatz 3.1 a-d -> d richtig?

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -7,109 +7,71 @@ Create arrays of n ∈ [100, 1000, 10000, 100 000] random numbers with uniform d
 Plot the raw data, then generate and plot histograms with 10 bins. How do the mean,
 minimum and maximum values of the bins (occupation counts) behave?
 '''
+def uniform(i,input_set):
+    return np.random.uniform(low=0.0, high=1.0, size=input_set[i]), input_set[i]
 
-def plot_uniform(set):
-    for i in set:
-        #mu, sigma = 0, 0.1 
-        #s = np.random.normal(mu, sigma, i)
-        s= np.random.uniform(low=0.0, high=1.0, size=i)
-        fig = plt.figure()
-        ax = fig.add_subplot(121)
-        ax.hist(s, bins=10)
-        ax2= fig.add_subplot(122)
-        ax2.plot(s)
-        fig.suptitle(i)
-    plt.show()
+def plot(func, input_set, *args):
+    fig = plt.figure(figsize=(20, 20))
+    fig.canvas.set_window_title(func.__name__)
+    outer = gridspec.GridSpec(len(input_set)//2+1, 2, wspace=0.2, hspace=0.2)
     
-def plot_uniform2(set):
-    fig = plt.figure(figsize=(10, 8))
-    outer = gridspec.GridSpec(len(set)//2+1, 2, wspace=0.2, hspace=0.2)
-    
-    for i in range(len(set)):
-        inner = gridspec.GridSpecFromSubplotSpec(1, 2,
+    for i in range(len(input_set)):
+        inner = gridspec.GridSpecFromSubplotSpec(1, 3,
                         subplot_spec=outer[i], wspace=0.1, hspace=0.1)
-        
-        #mu, sigma = 0, 0.1 
-        #s = np.random.normal(mu, sigma, i)
-        s= np.random.uniform(low=0.0, high=1.0, size=set[i])
+        s, k= func(i,input_set, *args)
 
         ax = plt.Subplot(fig, inner[0])
-        ax.set_title('k = {0:d}'.format(set[i]))
-        ax.hist(s, bins=10)
-        fig.add_subplot(ax)
-        ax1 = plt.Subplot(fig, inner[1])
-        ax1.set_title('k = {0:d}'.format(set[i]))
-        ax1.plot(s)
-        fig.add_subplot(ax1)
-
-    plt.show()
-
-def uniform(x,i):
-    return np.random.uniform(low=0.0, high=1.0, size=x[i]), x[i]
-
-def plot(func, *set):
-    fig = plt.figure(figsize=(10, 8))
-    outer = gridspec.GridSpec(len(*set)//2+1, 2, wspace=0.2, hspace=0.2)
-    
-    for i in range(len(*set)):
-        inner = gridspec.GridSpecFromSubplotSpec(1, 2,
-                        subplot_spec=outer[i], wspace=0.1, hspace=0.1)
-        s, k= func(*set, i)
-
-        ax = plt.Subplot(fig, inner[0])
-        ax.set_title('k = {0:d}'.format(k))
+        ax.set_title('n = {0:d}'.format(k))
         ax.hist(s, bins=10)
         fig.add_subplot(ax)
         ax1 = plt.Subplot(fig, inner[1])
         #ax1.set_title('k = {0:d}'.format(k))
         ax1.plot(s)
         fig.add_subplot(ax1)
-
-    plt.show()
+        ax2 = plt.Subplot(fig, inner[2])
+        #ax2.set_title('k = {0:d}'.format(k))
+        ax2.scatter(s, range(len(s)))
+        fig.add_subplot(ax2)
     
-
-    
+# plot(uniform, ([100, 1000, 10000, 100000]))
+        
 '''
 b. Create random numbers from a Gaussian distribution with mean μ and variance σ2. Plot
 the raw data, then generate and plot histograms.
-'''
-    
-def plot_gauss(set, mean, sigma): 
-    #sigma== standard deviation
-    fig = plt.figure(figsize=(10, 8))
-    outer = gridspec.GridSpec(len(set)//2+1, 2, wspace=0.2, hspace=0.2)
-    
-    for i in range(len(set)):
-        inner = gridspec.GridSpecFromSubplotSpec(1, 3,
-                        subplot_spec=outer[i], wspace=0.1, hspace=0.1)
-        
-        s = np.random.normal(mean, sigma, set[i])
-
-        ax = plt.Subplot(fig, inner[0])
-        ax.set_title('k = {0:d}'.format(set[i]))
-        ax.hist(s, bins=10)
-        fig.add_subplot(ax)
-        ax1 = plt.Subplot(fig, inner[1])
-        ax1.set_title('k = {0:d}'.format(set[i]))
-        ax1.plot(s)
-        fig.add_subplot(ax1)
-        ax2 = plt.Subplot(fig, inner[2])
-        ax2.set_title('k = {0:d}'.format(set[i]))
-        ax2.scatter(s, range(len(s)))
-        fig.add_subplot(ax2)
-    plt.show()
+'''  
+def gauss(i,input_set, args):
+    mu, sigma = args[0], args[1]
+    return np.random.normal(mu, sigma, input_set[i]), input_set[i]
+# plot(gauss, [100, 1000, 10000, 100000], (0, 0.1))
     
 '''
 c. As before, but using the Binomial distribution with parameters n and p.
 '''
+def binomial(i, input_set, args):
+    n, p = args[0], args[1]
+    return np.random.binomial(n, p, input_set[i]), input_set[i]
 
-
+'''
+d. Maybe combining multiple random numbers is even better than using single ones?
+Use numpy to generate new random numbers from a sum of individual numbers, si =
+PMj=1 rj , where the rj are generated from a uniform distribution. Plot scatter plots and
+histograms of the resulting data sets for M ∈ [2, 3, 5, 10, 20].
+'''
+def individual(i, input_set, args):
+    a = np.zeros(args)
+    for j in range(input_set[i]):
+        b = np.random.uniform(low=0.0, high=1.0, size=args)
+        #b.sort() # TODO richtig?
+        a = a + b
+    return a, input_set[i]
+        
 
 def main():
-   #plot_uniform([100, 1000, 10000, 100000]) 
-   #plot_uniform2([100, 1000, 10000, 100000])
-   #plot_gauss([100, 1000, 10000, 100000], 0, 0.1)
-   plot(uniform, [100, 1000, 10000, 100000])
+    plot(uniform, ([100, 1000, 10000, 100000]))
+    plot(gauss, [100, 1000, 10000, 100000], (0, 0.1)) # (0,0.1) -> (mean, variance)
+    plot(binomial, [100, 1000, 10000, 100000], (10, 0.5)) # ((10, 0.5) -> (n, p)
+    plot(individual, [2, 3, 5, 10, 20], 1000) # 1000 -> größe der uniform distributions die aufaddiert werden)
+    plt.show()
 
 if __name__ == "__main__":
   main()
