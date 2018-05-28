52d41497e614215eb1054b16c85ecee655143550
Marcel Rohde <2rohde@informatik.uni-hamburg.de#>
2018-05-28 13:57:04 +0200

    todos

--- Assignment6/assignment6.py
+++ Assignment6/assignment6.py
@@ -6,7 +6,6 @@ import timeit
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn.metrics import zero_one_loss
-from matplotlib.pyplot import plot
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 
@@ -58,7 +57,8 @@ for c in Cs:
          multi_class='ovr', penalty='l2', random_state=0, tol=0.0001,
          verbose=0)
     model = clf.fit(input_train, target_train)
-    error = 1- model.score(input_train, target_train) #TODO predict(train_data) -> zeroloss(trainlabel)
+    #error = 1- model.score(input_train, target_train)
+    error = zero_one_loss(target_train, clf.predict(input_train))
     errors.append(error)
     prediction = clf.predict(input_test)
     t_error = zero_one_loss(target_test, prediction)
@@ -68,7 +68,7 @@ for c in Cs:
 plt.figure(fignum)
 plt.plot(range(len(Cs)), errors, label ='train_error')
 plt.plot(range(len(Cs)), test_error, label='test_error')
-legend = plt.legend(loc='upper center', shadow=True)
+plt.legend(loc='upper center', shadow=True)
 plt.ylim(0, 0.05)
 plt.xticks(np.arange(7), Cs)
 plt.xlabel('C')
@@ -99,7 +99,8 @@ def test_kernels(t, t_label, plottrain, title):
                       probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, 
                       max_iter=-1, decision_function_shape='ovr', random_state=None)
             model = clf.fit(t, t_label)
-            error = 1- model.score(t, t_label) # todo predict, loss,...
+            #error = 1- model.score(t, t_label)
+            error = zero_one_loss(target_train, clf.predict(input_train))
             errors.append(error)
             print(kern, c, "train:", error)
             prediction = clf.predict(input_test)
@@ -109,7 +110,7 @@ def test_kernels(t, t_label, plottrain, title):
         if plottrain:
             plt.plot(range(len(Cs)), errors, label=kern+ ' train')
         plt.plot(range(len(Cs)), test_error, label=kern+ ' test')
-    legend = plt.legend(loc='upper center', shadow=True)
+    plt.legend(loc='upper center', shadow=True)
     plt.xticks(np.arange(7), Cs)
     plt.title(title)
     plt.xlabel('C')
@@ -145,27 +146,30 @@ reg.fit(train_data,train_label)
 #print(test_label)
 
 def knn(d, n, k):
-    #TODO Dimension?
-    # TODO für USPS oder für cancer-data?
     ran = range(0,len(train_data))
     sample = random.sample(ran, n)
-    t = [train_data[x] for x in sample]
+    t = [train_data[x][:d] for x in sample]
     tl = [train_label[x] for x in sample]
     start = timeit.default_timer()
     knn = KNeighborsRegressor(k)
     knn.fit(t, tl)
-    knn.predict(test_data)
+    b = [test_data[x][:d] for x in range(0,len(test_data))]
+    knn.predict(b)
+    #knn.predict(test_data)
     stop = timeit.default_timer()
     print ('knn d=',d,' n=',n,' k=',k, ' ->' , stop - start)
     
 knn(1, 1000, 5)
 for n in [100,500, 1000, 3000, 5000, 7000, 10000]:
     for k in [1,2,3,5,10]:
-        knn(1, n, k)
+        for d in [1,10,100,255]:
+            knn(d, n, k)
         
 #clf = SVC(decision_function_shape='ovr') # ovr = one-vs-rest,  ovo = one-vs-one
-# TODO Werte O(nd+kn)
 '''
+The estimated time values should be in O(nd+kn)
+
+
 What is the computational complexity of predicting a new data point for a SVM?  (m support
 vectors after training)
 
@@ -174,20 +178,19 @@ just a single inner product. Prediction complexity of kernel SVM depends on the 
 kernel and is typically proportional to the number of support vectors. For most kernels,
 including polynomial and RBF, this is O(nSVd) where nSV is the number of support vectors.
 
-TODO O(md) running time
-
 
 How much information do you need to store for predicting with each of these methods (space
 complexity)?
-TODO
-O(nd) für knn und O(md) für SVM
+Knn needs O(nd), SVM needs O(md)
 
 For a specific example, consider a one-against-the-rest classifier for the full USPS dataset
 (d = 256, n = 10000), assume k = 10 for kNN and m = 1000 support vectors for the SVM
 classifier. How many operations and how much memory is needed?
-TODO
 
-TODO mit O(nd) usw für computation und space ausrechen für knn und SVM
+operations knn: O(nd + kn) = 10000 * 256 + 10 * 10000 = 2660000
+memory knn: O(nd) = 10000 * 256 = 2560000
+operations SVM: O(nSVd) = O(md) = 1000 * 256 = 256000
+memory SVM: O(md) = 1000 * 256 = 256000
 
 
 '''
