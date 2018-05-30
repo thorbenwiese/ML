9cd0ea2128c443ef10b8bb1b8d75782f67adb0d0
Marcel Rohde <2rohde@informatik.uni-hamburg.de#>
2018-05-30 13:10:37 +0200

    Pfad für Daten geändert

--- Assignment7/face_recognition.py
+++ Assignment7/face_recognition.py
@@ -49,7 +49,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 ###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
 
-lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+lfw_people = fetch_lfw_people(data_home='./data', min_faces_per_person=70, resize=0.4)
 
 # introspect the images arrays to find the shapes (for plotting)
 n_samples, h, w = lfw_people.images.shape
