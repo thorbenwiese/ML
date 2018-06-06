048d3cda9e673b173c5a7190da416ddf30796090
Thorben Wiese <thorben.wiese@outlook.com>
2018-06-06 17:17:18 +0200

    Aufgabe 8.1 a

---
+++ Assignment8/assignment8.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+'''
+Aufgabe 8.1
+a)
+
+The 7 different matrices are used for different kinds of original sparse 
+matrices.
+
+DIA - Diagonal Format
+LIL - List of Lists Format
+DOK - Dictionary of Keys Format
+COO - Coordinate Format
+CSR - Compressed Sparse Row Format
+CSC - Compressed Sparse Column Format
+BSR - Block Compressed Row Format
+
+csr_matrix is the Compressed Sparse Row Format (CSR)
+The data is stores as arrays of rows and their values at each column
+Very efficient at accessing row-vectors or performing row-operations
+
+'''
