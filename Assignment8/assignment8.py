dd3e71494e2f3123c9c9b5c05ee77252654477be
Thorben Wiese <thorben.wiese@outlook.com>
2018-06-06 17:31:24 +0200

    Aufgabe 8.1 c)

--- Assignment8/assignment8.py
+++ Assignment8/assignment8.py
@@ -19,4 +19,15 @@ csr_matrix is the Compressed Sparse Row Format (CSR)
 The data is stores as arrays of rows and their values at each column
 Very efficient at accessing row-vectors or performing row-operations
 
+
+c)
+
+The scipy.sparse.find(A) functions returns the row indices, column indices and 
+values of the nonzero matrix A entries. One can use numpy.ma.size(A) to get all 
+elements and then calculate
+
+i, j, v = scipy.sparse.find(A)
+sparsity = (len(v) * 1.0) / numpy.ma.size(A)
+
+to get the sparsity percentage.
 '''
