29a5e7cc1ba8e87225b715ecb8ff989e416f84cd
Thorben Wiese <thorben.wiese@outlook.com>
2018-06-25 11:41:46 +0200

    Assignment 10

---
+++ Assignment10/assignment10.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# read CSV file and return columns
+def readCSVFile(f, train):
+
+  ids = []
+  urls = []
+  landmark_ids = []
+  with open(f) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+      ids.append(row[0])
+      urls.append(row[1])
+      if train == True:
+        landmark_ids.append(row[2])
+
+  if train == True:
+    return ids[:100], urls[:100], landmark_ids[:100]
+
+  return ids[:100], urls[:100]
+
+
+
+# use PILLOW to read images from url
+# Image.open(urllib2.urlopen(url))
+# Image.open(requests.get(url, stream=True).raw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+  print('Reading files...')
+  index_ids, index_urls = readCSVFile("landmark-recog/google-landmarks-dataset/index.csv", False)
+  train_ids, train_urls, train_landmark_ids = readCSVFile("landmark-recog/google-landmarks-dataset/train.csv", True)
+  test_ids, test_urls = readCSVFile("landmark-recog/google-landmarks-dataset/test.csv", False)
+  print('Files Read.')
+  
+
+
+
+
+# execute main
+if __name__ == "__main__":
+  main()
