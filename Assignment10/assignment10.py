19304c55873b37b74512264d4884b68b9f1073ac
Thorben Wiese <thorben.wiese@outlook.com>
2018-06-28 11:20:17 +0200

    Analysis

--- Assignment10/assignment10.py
+++ Assignment10/assignment10.py
@@ -5,6 +5,8 @@ import csv
 import scipy
 from scipy import misc
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 # read CSV file and return columns
 def readCSVFile(f, train):
@@ -69,6 +71,28 @@ def dimReduction(img):
 
 def main():
 
+  train_data = pd.read_csv('./landmark-recog/google-landmarks-dataset/train.csv')
+  test_data = pd.read_csv('./landmark-recog/google-landmarks-dataset/test.csv')
+
+  print "Train data size", train_data.shape
+  print "Test data size", test_data.shape
+
+  # Occurance of landmark_id in decreasing order(Top categories)
+  temp = pd.DataFrame(train_data.landmark_id.value_counts().head(5))
+  print temp
+  temp.reset_index(inplace=True)
+  temp.columns = ['Landmark_ID','Occurence']
+
+  # Plot the most frequent landmark_ids
+  plt.figure(figsize = (7, 5))
+  plt.title('Landmark_ID Occurences')
+  sns.barplot(x="Landmark_ID", y="Occurence", data=temp,
+            label="Occurence")
+
+  print temp
+
+
+
   print('Reading files...')
   #index_ids, index_urls = readCSVFile("landmark-recog/google-landmarks-dataset/index.csv", False)
   #train_ids, train_urls, train_landmark_ids = readCSVFile("landmark-recog/google-landmarks-dataset/train.csv", True)
@@ -77,7 +101,7 @@ def main():
   
   imgId = 'fdbac32ad9bbf6e5'
   try:
-    img = misc.imread('/Users/wiese/Downloads/images/' + imgId + '.jpg')
+    img = misc.imread('/Volumens/WIESE/landmark-images/train/' + imgId + '.jpg')
     x = getStatistics(img)
     plt.imshow(img, cmap=plt.cm.gray)
     plt.show()
@@ -85,7 +109,12 @@ def main():
     print 'Image with ID:', imgId, 'could not be read!'
 
 
+'''
+We selected the distribution of landmarks, the dimenions of the images, the amount of pixels, the pixel range (from min to max) and the pixel variance (max - min) to get a better understanding of what the images' shapes are.
+'''
+
 
 # execute main
 if __name__ == "__main__":
   main()
+  plt.show()
