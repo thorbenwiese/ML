ae116365639ac40d1e91c5808bb7fd47ef9130c9
Marcel Rohde <2rohde@informatik.uni-hamburg.de#>
2018-07-01 17:53:21 +0200

    pca

--- Assignment10/result2csv_without_landmarks.py
+++ Assignment10/result2csv_without_landmarks.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 
 p = "result.txt"
-result = [['id','shape0','shape1','shape2', 'max', 'min', 'variance', 'numPixels', 'landmark_id']]
+result = [['id','shape0','shape1','shape2', 'max', 'min', 'variance', 'numPixels']]
 train_data = pd.read_csv('./landmark-recog/google-landmarks-dataset/train.csv')
 
 with open(p, "r") as fp:
@@ -12,7 +12,6 @@ with open(p, "r") as fp:
         tmp[1:8] = [int(x) for x in tmp[1:8]]
         try:
             result.append(tmp)
-            #result.append((eval(tmp[0]), eval(tmp[1])))
         except:pass
 
 with open("result_without_landmarks.csv", "w") as f:
