77f9659b7820f1d381c647ce71f2d49837798657
Thorben Wiese <thorben.wiese@outlook.com>
2018-07-03 07:04:24 +0200

    More text and Feature Vectors

--- Assignment11/assignment11.py
+++ Assignment11/assignment11.py
@@ -8,47 +8,60 @@ import imutils
 import cv2
 import os
 import imageio
+import csv
 
-
+'''
 def image_to_feature_vector(image, size=(32, 32)):
 	# resize the image to a fixed size, then flatten the image into
 	# a list of raw pixel intensities
 	return cv2.resize(image, size).flatten()
+'''
 
+# Feature extractor
+def extract_features(image, vector_size=32):
+    try:
+        # Using KAZE, cause SIFT, ORB and other was moved to additional module
+        # which is adding addtional pain during install
+        alg = cv2.KAZE_create()
+        # Dinding image keypoints
+        kps = alg.detect(image)
+        # Getting first 32 of them. 
+        # Number of keypoints is varies depend on image size and color pallet
+        # Sorting them based on keypoint response value(bigger is better)
+        kps = sorted(kps, key=lambda x: -x.response)[:vector_size]
+        # computing descriptors vector
+        kps, dsc = alg.compute(image, kps)
+        # Flatten all of them in one big vector - our feature vector
+        dsc = dsc.flatten()
+        # Making descriptor of same size
+        # Descriptor vector size is 64
+        needed_size = (vector_size * 64)
+        if dsc.size < needed_size:
+            # if we have less the 32 descriptors then just adding zeros at the
+            # end of our feature vector
+            dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
+    except cv2.error as e:
+        print 'Error: ', e
+        return None
 
-def extract_color_histogram(image, bins=(8, 8, 8)):
-	# extract a 3D color histogram from the HSV color space using
-	# the supplied number of `bins` per channel
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
-		[0, 180, 0, 256, 0, 256])
- 
-	# handle normalizing the histogram if we are using OpenCV 2.4.X
-	if imutils.is_cv2():
-		hist = cv2.normalize(hist)
- 
-	# otherwise, perform "in place" normalization in OpenCV 3 (I
-	# personally hate the way this is done
-	else:
-		cv2.normalize(hist, hist)
- 
-	# return the flattened histogram as the feature vector
-	return hist.flatten()
-
+    return dsc
 
 imgPath = '/Users/wiese/Documents/UHH/Master/4.Semester/ML/Assignment11/selectedImages/'
+
 train_ids = [f.replace('.jpg','') for f in os.listdir(imgPath) if os.path.isfile(os.path.join(imgPath,     f))]
 
-#print train_ids
-
-imgVectors = []
+imgVectors = [['id','featureVector']]
+count = 0
 
 for imgId in train_ids:
+  print 'Run', count
+  count += 1
   try:
     img = imageio.imread(imgPath + imgId + '.jpg')
-    imgVectors.append(image_to_feature_vector(img))
-    #print extractor_color_histogram(img)
+    imgVectors.append([imgId, extract_features(img)])
   except:
     print 'COULD NOT READ IMAGE WITH ID:', imgId
 
-
+with open("result.csv", "w") as f:
+  writer = csv.writer(f)
+  writer.writerows(imgVectors)
