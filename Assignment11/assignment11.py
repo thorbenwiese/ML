f7439b8360d05bbeab62c1b736c53f1fa6b3aa79
Thorben Wiese <thorben.wiese@outlook.com>
2018-07-03 22:29:25 +0200

    Removed unused code

--- Assignment11/assignment11.py
+++ Assignment11/assignment11.py
@@ -1,22 +1,8 @@
-# import the necessary packages
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from imutils import paths
 import numpy as np
-import argparse
-import imutils
 import cv2
 import os
 import imageio
 import csv
-import matplotlib.pyplot as plt
-
-'''
-def image_to_feature_vector(image, size=(32, 32)):
-	# resize the image to a fixed size, then flatten the image into
-	# a list of raw pixel intensities
-	return cv2.resize(image, size).flatten()
-'''
 
 # Feature extractor
 def extract_features(image, vector_size=32):
@@ -53,8 +39,9 @@ train_ids = [f.replace('.jpg','') for f in os.listdir(imgPath) if os.path.isfile
 
 count = 0
 
-thresh = 500
-plt.figure()
+# thresh can be used to only select a couple of images
+thresh = len(train_ids)
+
 with open("result2.csv", "w") as f:
   writer = csv.writer(f)
   for imgId in train_ids:
@@ -67,8 +54,8 @@ with open("result2.csv", "w") as f:
       try:
         img = imageio.imread(imgPath + imgId + '.jpg')
         features = extract_features(img)
-        plt.plot(features)
+        print imgId
+        print [f for f in features]
         writer.writerow((imgId, [f for f in features]))
       except:
         print 'COULD NOT READ IMAGE WITH ID:', imgId
-plt.show()
