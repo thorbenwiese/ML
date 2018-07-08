6dc40a1d7c4e16f5fed2dc73864884089748b942
Marcel Rohde <2rohde@informatik.uni-hamburg.de#>
2018-07-08 17:11:46 +0200

    knn plot and result-log

--- Assignment11/knn.py
+++ Assignment11/knn.py
@@ -2,19 +2,28 @@
 import dataAdapter
 import matplotlib.pyplot as plt
 import time
+import sys
 from sklearn.neighbors import KNeighborsClassifier
 
+f = open('knn_result','w')
+sys.stdout = f
+
 plotfigures = False
-iterations = [200, 2000]#, 20000]
+iterations = [200, 200, 20000]
 ks = [2,3,4,5, 6, 7, 8, 9, 10, 15, 20, 30, 50, 75, 100, 125, 150, 200, 500, 750, 1000] # > 150 doesn't work for iteration = 200
+
+result = {'acc':[], 'time':[]}
 
 for iteration in iterations:
     #ids, vectors, labels = dataAdapter.readCSV('') # -> '' is default if filename is resultTRAIN.csv instead resultTRAIN<iteration>.csv
     #test_ids, test_vectors, test_labels = dataAdapter.readTestCSV('')
+    result['acc'].append([])
+    result['time'].append([])
 
     ids, vectors, labels = dataAdapter.readCSV( iteration)
     test_ids, test_vectors, test_labels = dataAdapter.readTestCSV(iteration)
 
+    '''
     # PLOTTING IMAGES -> TODO: Sind die sinnvoll?
     if plotfigures:
         plt.figure()
@@ -22,6 +31,7 @@ for iteration in iterations:
 
         plt.figure()
         [plt.plot(v) for v in vectors]
+    '''
 
 
     # PREDICTION WITH KNN
@@ -38,11 +48,29 @@ for iteration in iterations:
         #acc = model.score(test_vectors, tmp)
         acc = model.score(test_vectors, test_labels)
         end = time.time()
+        duration = end - start
+        result['acc'][-1].append(acc)
+        result['time'][-1].append(duration)
         print ( 'Configuration: size = {}, k= {}'.format(iteration, k))
         print ('KNN Accuracy:', acc)
-        print ('Time consumption: ', end - start)
+        print ('Time consumption: ', duration)
         print ( 30*'-')
-
+    '''
     if plotfigures:
         plt.show()
+    '''
+print(result)
+
+fig, ax = plt.subplots(2, sharex=True)
+for a in range(len(result['acc'])):
+    ax[0].plot(result['acc'][a], label = 'iteration = {}'.format(iterations[a]))
+    ax[1].plot(result['time'][a], label='iteration = {}'.format(iterations[a]))
+plt.xlabel('k')
+ax[0].set_title('acc')
+ax[1].set_title('time')
+ax[0].set_xticklabels(ks)
+ax[1].set_xticklabels(ks)
+legend = plt.legend()
+fig.savefig('knn.png')   # save the figure to file
+plt.close(fig)
 
